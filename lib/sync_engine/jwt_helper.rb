2c83f93aa11fe22babacf2d66a088e68d9d8c138
Johnny A <5891646+johnny243@users.noreply.github.com>
2020-09-30 16:46:17 -0700

    feat: legacy secret key base (#112)
    * feat: legacy secret key base
    
    * fix: env var name
    
    Co-authored-by: Johnny Almonte <johnny243@users.noreply.github.com>

--- lib/sync_engine/jwt_helper.rb
+++ lib/sync_engine/jwt_helper.rb
@@ -6,6 +6,10 @@ module SyncEngine
       Rails.application.secrets.secret_key_base
     end
 
+    def self.legacy_secret_key_base
+      ENV['LEGACY_SECRET_KEY_BASE']
+    end
+
     def self.encode(payload)
       JWT.encode(payload, secret_key_base, 'HS256')
     end
@@ -13,9 +17,13 @@ module SyncEngine
     def self.decode(token)
       decoded_token = JWT.decode(token, secret_key_base, true, algorithm: 'HS256')[0]
       HashWithIndifferentAccess.new(decoded_token)
-    rescue StandardError => e
-      puts e
-      nil
+    rescue StandardError
+      begin
+        decoded_token = JWT.decode(token, legacy_secret_key_base, true, algorithm: 'HS256')[0]
+        HashWithIndifferentAccess.new(decoded_token)
+      rescue StandardError
+        nil
+      end
     end
   end
 end
